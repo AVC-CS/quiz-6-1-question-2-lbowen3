2259c527ad0056eb6f8fb7a70c7d142119649436
Luke Bowen <lbowen3@students.avc.edu>
2024-03-15 13:05:45 -0700

    Started Code

--- main.hpp
+++ main.hpp
@@ -1,3 +1,19 @@
+#include <iostream>
+using namespace std;
+
 /***************************************************
  * Code your program here
  ***************************************************/
+
+int writeFile(string);
+int readFile(string); 
+
+int writeFile(string filename)
+{
+    
+}
+
+int readFile(string filename)
+{
+
+}
