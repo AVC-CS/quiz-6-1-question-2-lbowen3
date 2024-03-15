832a5a8211524de3581a4c4d35f3303db62aa085
lbowen3 <144710267+lbowen3@users.noreply.github.com>
2024-03-15 20:18:38 +0000

    Edited Code

--- main.hpp
+++ main.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 /***************************************************
@@ -8,9 +9,32 @@ using namespace std;
 int writeFile(string);
 int readFile(string); 
 
+ofstream ofs;
+
 int writeFile(string filename)
 {
-    
+   ofs.open(filename);
+   if (!ofs){
+    cerr << "File open Error \n";
+    exit(0);
+   }
+
+    int empNum;
+    cout << "Input number of employees: ";
+    cin >> empNum;
+    ofs << empNum << endl;
+
+    int empID, empName, empDep, empSal;
+
+    for (int i=0; i<empNum; i++)
+    {
+        cout << "Input employee ID, Name, Department, and Salary: ";
+        cin >> empID >> empName >> empDep >> empSal;
+        ofs << empID << " " << empName << " " << empDep << " " << empSal << endl;
+    }
+
+    return empNum;
+   
 }
 
 int readFile(string filename)
